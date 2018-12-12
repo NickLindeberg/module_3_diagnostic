bd4fbb97903fb83c121ad62e86e195c6da6abfb4
Nick Lindeberg <lindebergnick@gmail.com>
2018-12-12 10:53:25 -0700

    got a new error due to not connecting to the api correctly and parsing error looking with pry to see what i can fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,16 @@
 class SearchController < ApplicationController
 
   def index
-    @facade = SearchFacade.new(params[:zip])
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json") do |faraday|
+      faraday.headers["api_key"] = ENV["API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+require "pry"; binding.pry
+    response = @conn.get("/fuel_type=ELEC%26LPG&state=CO&limit=10&format=JSON&zip=80203")
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    @stations = results.map do |station|
+      Station.new(station)
+    end
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -14,17 +14,5 @@ class Station
     @access_times   = attributes[:access_times].to_i
   end
 
-  def find_stations(zip)
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json") do |faraday|
-      faraday.headers["api_key"] = ENV["API_KEY"]
-      faraday.adapter Faraday.default_adapter
-    end
-require "pry"; binding.pry
-    response = @conn.get("/fuel_type=ELEC%26LPG&state=CO&limit=10&format=JSON&zip=#{zip}")
-    results = JSON.parse(response.body, symbolize_names: true)[:results]
-
-    @stations = results.map do |station|
-      Station.new(station)
-    end
-  end
+  
 end
